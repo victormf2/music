ed0f02329cd5717b7722a0415778acbc5344b83d
Neusa <Neusa@localhost>
2013-03-29 09:33:49 -0300

    slider meio pronto

--- js/jquery-css-slider.js
+++ js/jquery-css-slider.js
@@ -3,45 +3,96 @@
         init: function(options) {
             var settings = $.extend({
                 interval: 3000,
+                duration: 500,
                 step: 1,
-                height: 'guess'
+                show: 4,
+                itemHeight: 'guess',
+                itemWidth: 'guess',
+                cyclic: false
             }, options);
             return this.each(function() {
                 var $ul = $(this);
                 $ul.data('css-slider.settings', settings);
-                $ul.addClass('css-slider');
-                var height = settings.height;
-                if (height === 'guess') {
-                    height = $ul.find('li').outterHeight();
+                var $lis = $ul.find('li').css('float', 'left');
+
+                var height = settings.itemHeight;
+                if (isNaN(height)) {
+                    height = $lis.outerHeight(true);
+                    settings.itemHeight = height;
                 }
+                var itemWidth = settings.itemWidth;
+                var width = itemWidth * settings.show;
+                if (isNaN(width)) {
+                    itemWidth = $lis.outerWidth(true);
+                    settings.itemWidth = itemWidth;
+                    width = itemWidth * settings.show;
+                }
+                var duration = settings.duration / 1000;
+                $ul.css({
+                    height: height + 'px',
+                    width: ($lis.length * itemWidth + 2 * itemWidth) + 'px',
+                    transition: 'left ' + duration + 's, right ' + duration + 's'
+                }).wrap('<div></div>').parent().addClass('css-slider').css('width', width + 'px');
+                $ul.addClass('css-slider').show().css({visibility: 'visible'})
+                        .data('css-slider.limit', $lis.length - settings.show);
                 if (settings.interval) {
-                    var interval = setInterval(function() {
-                        $ul.cssSlider('slide', 'next');
-                    }, settings.interval);
-                    $ul.data('css-slider.interval', interval);
+                    $ul.cssSlider('proceed');
                 }
             });
         },
         slide: function(direction) {
             return this.each(function() {
                 var $ul = $(this);
-                var step = $ul.data('css-slider.settings').step;
+                var settings = $ul.data('css-slider.settings');
+                var step = settings.step;
                 var $lis = $ul.find('li');
                 var length = $lis.length;
-                if (step >= length) return;
+                if (step >= length)
+                    return;
                 switch (direction) {
                     case 'next':
-                        var $last = $lis.filter(':last');
-                        var $firsts = $lis.slice(0, step);
-                        $firsts.after($last);
+                        if (!settings.cyclic) {
+                            var limit = $ul.data('css-slider.limit');
+                            var position = Math.abs($ul.position().left);
+                            var hiddens = position / settings.itemWidth;
+                            var nextPosition = hiddens >= limit ? 0 : position + settings.itemWidth;
+                            $ul.css('left', '-' + nextPosition + 'px');
+                        } else {
+                            var $last = $lis.filter(':last');
+                            var $firsts = $lis.slice(0, step);
+                            $last.after($firsts);
+                        }
                         break;
                     case 'prev':
-                        var $first = $lis.filter(':last');
-                        var $lasts = $lis.slice(length - step, length);
-                        $lasts.before($first);
+                        if (!settings.cyclic) {
+                            
+                        } else {
+                            var $first = $lis.filter(':last');
+                            var $lasts = $lis.slice(length - step, length);
+                            $first.before($lasts);
+                        }
                         break;
                 }
-                
+
+            });
+        },
+        stop: function() {
+            return this.each(function() {
+                var $ul = $(this);
+                clearInterval($ul.data('css-slider.interval'));
+            });
+        },
+        proceed: function() {
+            return this.each(function() {
+                var $ul = $(this);
+                var settings = $ul.data('css-slider.settings');
+                setTimeout(function() {
+                    $ul.cssSlider('slide', 'next');
+                    var interval = setInterval(function() {
+                        $ul.cssSlider('slide', 'next');
+                    }, settings.interval + settings.duration);
+                    $ul.data('css-slider.interval', interval);
+                }, settings.interval);
             });
         }
     };
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+    $('.music-list-article ul').cssSlider({
+        interval: 1000,
+        duration: 1000
+    });
+});
