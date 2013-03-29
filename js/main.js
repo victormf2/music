c180f9620044bc97aa6c3a6fd208a17bf6fdf160
Neusa <Neusa@localhost>
2013-03-29 11:10:06 -0300

    jquery-css-slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 $(window).load(function() {
+    $('.overlay').on('click', function() {
+        $(this).hide();
+        $('.overlay-content').remove();
+    });
     $('.music-list-article ul').cssSlider({
         interval: 1000,
         duration: 1000
     });
+    $('.music-list-article li > div').on('click', function() {
+        overlay('<iframe width="640" height="390" src="http://www.youtube.com/embed/' + $(this).parent().attr('data-url') + '?feature=player_detailpage&wmode=Opaque" frameborder="0" allowfullscreen="1"></iframe>');
+    });
 });
+
+function overlay(el) {
+    var $el = $(el);
+    var $overlay = $('.overlay').show();
+    var $div = $('<div class="overlay-content"></div>')
+    $div.append($el);
+    $div.css({'margin-top': '-195px', 'margin-left': '-320px'});
+    $overlay.after($div);
+}
